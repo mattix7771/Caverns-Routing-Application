fe6c9e749d48484f81adebd3b6f97bed6b485ccd
mattix7771 <mattione7@gmail.com>
2022-11-07 18:28:27 +0000

    initial commit

---
+++ Cave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caverns_Routing_Application
+{
+    class Cave
+    {
+        private int x {get;}
+        private int y {get;}
+
+        Cave(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Caverns_Routing_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get file location from argument
+            string path = args[0] + ".cav";
+
+            //Check if file exists
+            if(File.Exists(path)){
+                Console.WriteLine("File does not exist");
+                System.Environment.Exit(0);
+            }
+            
+            //Extract info from file
+            String[] all = File.ReadAllText(path).Split(',');
+
+            int cav_num = Int32.Parse(all[0]);
+            List<Cave> caves = new List<Cave>();
+
+            
+        }
+    }
+}
